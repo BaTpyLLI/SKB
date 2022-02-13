5fbf975d9ced43aa717391c5aa61b4879cf9df32
batpylli <batpylli@gmail.com>
2022-02-14 01:31:44 +0500

    Заменил проверку соответствия имени ветки на рабочий вариант regex + мелкие правки

--- script.sh
+++ script.sh
@@ -12,10 +12,10 @@ echo "" > temp.txt
 for Z in $branch;
 do
 #echo "Значение $Z"
-    if echo ${exception[@]} | grep "$Z" ;
-       then echo "$Z present in the array"
-       else echo "$Z" >> temp.txt #Заносим данные в файл, для дальнейшей обработки
-fi
+  if echo ${exception[@]} | grep "$Z" ;
+    then echo "$Z present in the array"
+    else echo "$Z" >> temp.txt #Заносим данные в файл, для дальнейшей обработки
+  fi
 done
 
 #Подгружаем имена веток, с которыми будем работать
@@ -25,19 +25,21 @@ processed=$(cat temp.txt)
 for i in $processed
  do
   #Проверяем, соответствует ли имя ветки регекспу
-  if [ $i != 1 ]
-    then
-    #Если имя ветки не проходит проверку, то определяется mail последнего вносившего изменения и отправляем ему письмо
-    mail=$(git log $i --format='%ae' -n1) && echo "$i" >> exception.txt
-    #mail=$(git log $i --format='%ae' -n1) && echo "Переименуй" | mail -s "Необходимо переименовать ветку $i" -a "Сервер через который пойдет письмо" -r *От какой почты письмо* "$mail" && echo "$i" >> exception.txt
-  fi
-  # Вычисляем время в Unix, когда последний раз обновлялась ветка
-  datecom=$(git log $i --format='%ad' -n1 | awk "{NF--} 1") && time=$(date -d "$datecom" +"%s")
-  #Если ветка не обновлялась более двух недель, то отсылаем письмо + убираем из обработки
-  if (( $date - $time  > 1209600 ))
-    then echo "Время обновлять"
-    #mail=$(git log $i --format='%ae' -n1) && echo "Необходимо обновить или удалить ветку $i" | mail -s "Обнови или удали $i" -a "Сервер через который пойдет письмо" -r *От какой почты письмо* "$mail" >> exception.txt
-    else echo "Еще не время обновлять"
-  fi
+   if [[ $i =~ ^(bugfix|feature)-task-[0-9] ]]
+     then
+       echo "Все ок, ветка $i соответвует"
+     else
+     #Если имя ветки не проходит проверку, то определяется mail последнего вносившего изменения и отправляем ему письмо
+     email=$(git log $i --format='%ae' -n1) && echo "Ветка $i не прошла проверку, письмо последнему контрибутору - $email" #exception.txt
+     #mail=$(git log $i --format='%ae' -n1) && echo "Переименуй" | mail -s "Необходимо переименовать ветку $i" -a "Сервер через который пойдет письмо" -r *От какой почты письмо* "$mail" && echo "$i" >> exception.txt
+   fi
+   # Вычисляем время в Unix, когда последний раз обновлялась ветка
+   datecom=$(git log $i --format='%ad' -n1 | awk "{NF--} 1") && time=$(date -d "$datecom" +"%s")
+   #Если ветка не обновлялась более двух недель, то отсылаем письмо + убираем из обработки
+   if (( $date - $time  > 1209600 ))
+     then email2=$(git log $i --format='%ae' -n1) && echo "Время обновлять, письмо на $email2"
+     #mail=$(git log $i --format='%ae' -n1) && echo "Необходимо обновить или удалить ветку $i" | mail -s "Обнови или удали $i" -a "Сервер через который пойдет письмо" -r *От какой почты письмо* "$mail" >> exception.txt
+     #else echo "Еще не время обновлять"
+   fi
 done
 
